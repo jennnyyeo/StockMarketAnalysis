fefa951ca33758a4ed848c261f0594638990abf8
kai-jun <109254091+kai-jun@users.noreply.github.com>
2025-09-19 19:32:46 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -1,13 +1,35 @@
 import pandas
 
-data = pandas.read_csv('stock/MSFT.csv')
+data = pandas.read_csv('MSFT.csv')
 
-# format data
+# Empty list to store cleaned values
+cleaned = []
+
 for x in data['Close/Last']:
-    data['Close/Last']=float(x[1:])
-  
+    cleaned.append(float(x[1:]))   # remove '$' and convert to float
+
+data['Close/Last'] = cleaned
 print(data.info())
 
-# 5-days SMA
-data['SMA'] = data['Close/Last'].rolling(window=5).mean()
-print(data) 
+
+
+
+def SMA(close,period):
+    sma=[]
+    
+    for i in range(len(close)):
+        if i<period-1: #index 0-3
+            sma.append(None)
+        else:   # index 4
+            total=0
+            for j in range(period):
+                total+=close[i-j] 
+            sma.append(total/period)
+           
+    return sma
+     
+
+data['custom_SMA']=SMA(data['Close/Last'],5) 
+# test validation
+data['test_SMA'] = data['Close/Last'].rolling(window=5).mean()
+print(data)
