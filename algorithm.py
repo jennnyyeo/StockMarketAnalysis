4a6cd5fc60189fe6bf2f484d18605a2c01b4b3f3
joelljq <114987148+joelljq@users.noreply.github.com>
2025-09-26 17:48:34 +0800

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -8,39 +8,43 @@ def bshalgorithm(data, streak_threshold=5, profit_threshold=0.05):
     data['Streaks'] = streakindetifier(data)  
     price_now = data['Close'].iloc[i] 
 
-    for i in range(data):
+    for i in range(len(data)):
         if i < 50:
             signals.append['Hold']
-
+            continue
         sma_20 = data['SMA_20'].iloc[i]
         sma_50 = data['SMA_50'].iloc[i]
         streak = data['Streaks'].iloc[i]
 
         sma_20_prev = data['SMA_20'].iloc[i-1]
-        sma_50_prev = data['SMA_50'].iloc[i]
+        sma_50_prev = data['SMA_50'].iloc[i-1]
 
         signal = 'Hold'
 
+        #Rule 1: SMA Crossover  
         if sma_20 > sma_50 and sma_20_prev <= sma_50_prev:
             signal = 'Buy'
 
         elif sma_20 < sma_50 and sma_20_prev >= sma_50_prev:
             signal = 'Sell'
-
-        if streak <= streak_threshold:
+            
+        #Rule 2: Streak Reversal
+        elif streak <= -streak_threshold:
             signal = 'Buy'
 
         elif streak >= streak_threshold:
             signal = 'Sell'
-        
-        pricelowrecent = min(data['Low'].iloc[i-5:i])
-        pricehighrecent = max(data['High'].iloc[i-5:i])
+            
+        #Rule 3: Dip-buying / Profit-Taking 
+        elif i > 5:
+            pricelowrecent = min(data['Low'].iloc[i-5:i])
+            pricehighrecent = max(data['High'].iloc[i-5:i])
 
-        if price_now > pricelowrecent*(1 + profit_threshold):
-            signal = 'Buy'
+            if price_now > pricelowrecent*(1 + profit_threshold):
+                signal = 'Buy'
 
-        elif price_now > pricehighrecent*(1 - profit_threshold):
-            signal = 'Sell'
+            elif price_now > pricehighrecent*(1 - profit_threshold):
+                signal = 'Sell'
 
         
         signals.append(signal)
@@ -51,9 +55,9 @@ def bshalgorithm(data, streak_threshold=5, profit_threshold=0.05):
         elif signal == 'Sell':
             sell_dates.append(data.index[i])
         
-        data['Signal'] = signal
+    data['Signal'] = signal
         
-        return data, buy_dates, sell_dates
+    return data, buy_dates, sell_dates
 
 
         
