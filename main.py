a8ee7c35acab4b7f48acc514b92a65b8b7b3a814
joelljq <114987148+joelljq@users.noreply.github.com>
2025-09-26 17:31:18 +0800

    newshit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import advice, algorithm, dailyReturns, plotCharts, start
+# from start import SMA
+# from algorithm import bshalgorithm
+# from dailyReturns import daily_returns
+# from plotCharts import plot_png
+from flask import Flask, Response, render_template, request
+
+app = Flask(__name__)
+
+results_cache = None
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
