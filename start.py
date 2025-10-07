cfb2730fe558e2bb80563bffaf85df35d50f3510
kai-jun <109254091+kai-jun@users.noreply.github.com>
2025-10-07 19:03:30 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -1,13 +1,25 @@
-import pandas as pd
+import pandas
+if __name__ == "__main__":
+    data = pandas.read_csv('MSFT.csv')
 
-## data = pandas.read_csv('MSFT.csv')
+# Empty list to store cleaned values
+cleaned = []
 
-'''
-corner cases, such as when the data series is shorter than
-the SMA window, or when one day’s data is missing.
-'''
+cleaned = [float(x[1:]) for x in data['Close/Last']]    # list comprehension
 
-def SMA(close,period):
+
+data['Close/Last'] = cleaned
+print(data.info())
+
+period = input('select SMA days(must be integer):')
+if period.isdigit() and 2<=int(period)<=len(data):
+    period = int(period)
+else:
+    print('your input is invalid select 2 or more days without exceeding data series')
+    exit()
+
+# function annotation
+def SMA(close:float,period:int):
     sma=[]
     
     for i in range(len(close)):
@@ -17,32 +29,13 @@ def SMA(close,period):
             total=0
             for j in range(period):
                 total+=close[i-j] 
-            sma.append(total/period)   
+            sma.append(total/period)
+           
     return sma
+     
 
-if __name__ == "__main__":
-    data = pd.read_csv('MSFT.csv')
-    # Empty list to store cleaned values
-    cleaned = []
+data['custom_SMA']=SMA(data['Close/Last'],5)    #positional argument
 
-    for x in data['Close/Last']:
-        cleaned.append(float(x[1:]))   # remove '$' and convert to float
-
-    data['Close/Last'] = cleaned
-    # print(data.info())
-
-    period = input('select SMA days(must be integer):')
-    if period.isdigit() and 2<=int(period)<=len(data):
-        period = int(period)
-    else:
-        print('your input is invalid select 2 or more days without exceeding data series')
-        exit()
-
-    # period 2 to last day
-
-
-    data['custom_SMA']=SMA(data['Close/Last'],period) 
-    # test validation
-    data['test_SMA'] = data['Close/Last'].rolling(window=period).mean()
-    print(data)
-
+# test validation
+data['test_SMA'] = data['Close/Last'].rolling(window=5).mean()
+print(data)
